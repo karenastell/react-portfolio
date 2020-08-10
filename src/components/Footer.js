37c5ae5cab138cdfdb1c5ce165f785b14ddb1a6d
Karen Astell <KarenAstell@gmail.com>
2020-08-10 18:02:19 -0400

    footer without font awesome

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,18 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <div>
-            
+        <div className="foot foot-font center" role="navigation" aria-label="main navigation">
+        <div className="content has-text-centered">
+          <p>
+            <strong className="white-font">Karen Astell</strong>
+            <i className="far fa-circle white-font"></i>
+            <span className="white-font bold">(603) 770 7658</span>
+            <i className="far fa-circle white-font"></i>
+            <a href="mailto:karenastell@gmail.com" target="_blank"
+              ><span className="white-font bold">KarenAstell@gmail.com</span>
+            </a>
+          </p>
         </div>
+      </div>
     )
 }
