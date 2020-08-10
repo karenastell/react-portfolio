625a1788d7e5c18d179b6033d0bfd47c9d73bfef
Karen Astell <KarenAstell@gmail.com>
2020-08-10 18:40:43 -0400

    fixed nav to bulma - still needs font awesome

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,40 +8,39 @@ import '../style.css';
 //  const element = <FontAwesomeIcon icon={fabGithub}/>
 export default function Navbar() {
   return (
-    <nav className='fence navbar navbar-expand-lg navbar-light bg-light'>
-      <div className='navbar-collapse' id='navbarTogglerDemo02'>
-        <div className='mr-auto'>
-          <Link className='white-font ml-1 mr-3 navbar-item' to='/'>
-            <span className=' navbar-font bold'>Home</span>
-          </Link>
-          <Link className='white-font navbar-item' to='/portfolio'>
-            <span className=' navbar-font bold'>Portfolio</span>
-          </Link>
-        </div>
+    <nav role='navigation' aria-label='main navigation'>
+      <div className='navbar-brand'>
+        <Link className='has-text-white ml-1 mr-3 navbar-item' to='/'>
+          <span className=' navbar-font bold'>Home</span>
+        </Link>
+        <Link className='has-text-white navbar-item' to='/portfolio'>
+          <span className=' navbar-font bold'>Portfolio</span>
+        </Link>
 
-        <span className='white-font navbar-item'>
-          <a
-            className=' bold nav-link-margin is-inverted'
-            href='https://github.com/karenastell'
-            target='_blank'
-          >
-            <span className='icon'>
-              <i className='fab fa-github'></i>
-            </span>
-            <span className='white-font navbar-font'>GitHub</span>
-          </a>
-          <a
-            className=' bold nav-link-margin'
-            target='_blank'
-            href='https://www.linkedin.com/in/karen-astell/'
-          >
-            <span className='icon'>
-              <i className='fab fa-linkedin'></i>
-            </span>
-            <span className='white-font navbar-font'>LinkedIn</span>
-          </a>
-          <a
-            className='white-font bold is-inverted'
+        <div className='navbar-end'>
+          <span className='navbar-item'>
+            <a
+              className='has-text-white bold nav-link-margin is-inverted'
+              href='https://github.com/karenastell'
+              target='_blank'
+            >
+              <span className='icon'>
+                <i className='fab fa-github'></i>
+              </span>
+              <span className='navbar-font'>GitHub</span>
+            </a>
+            <a
+              className='has-text-white bold nav-link-margin'
+              target='_blank'
+              href='https://www.linkedin.com/in/karen-astell/'
+            >
+              <span className='icon'>
+                <i className='fab fa-linkedin'></i>
+              </span>
+              <span className='navbar-font'>LinkedIn</span>
+            </a>
+            <a
+            className='has-text-white bold is-inverted'
             href={Resume}
             target='_blank'
           >
@@ -50,7 +49,8 @@ export default function Navbar() {
             </span>
             <span className='navbar-font'>Resume</span>
           </a>
-        </span>
+          </span>
+        </div>
       </div>
     </nav>
   );
