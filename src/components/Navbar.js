ab5a47fd1b1ce0d9a82d2b433296f809dbbc9141
Karen Astell <KarenAstell@gmail.com>
2020-08-11 11:53:55 -0400

    got icons for nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import {fabGithub} from '@fortawesome/free-brands-svg-icons'
+import { FaLinkedin } from "react-icons/fa";  // Font Awesome
+import { FaGithub } from "react-icons/fa";  // Font Awesome
+import { FaFilePdf } from "react-icons/fa";  // Font Awesome
 import Resume from '../files/KarenAstellResume.pdf';
 import '../style.css';
 
-//  const element = <FontAwesomeIcon icon={fabGithub}/>
+// const element = <FontAwesomeIcon icon={['fab', 'apple']} />;
+// const element = <FontAwesomeIcon icon={faCoffee} />;
 export default function Navbar() {
   return (
     <nav role='navigation' aria-label='main navigation'>
@@ -16,7 +18,7 @@ export default function Navbar() {
         <Link className='has-text-white navbar-item' to='/portfolio'>
           <span className=' navbar-font bold'>Portfolio</span>
         </Link>
-
+        
         <div className='navbar-end'>
           <span className='navbar-item'>
             <a
@@ -24,31 +26,26 @@ export default function Navbar() {
               href='https://github.com/karenastell'
               target='_blank'
             >
-              <span className='icon'>
-                <i className='fab fa-github'></i>
-              </span>
-              <span className='navbar-font'>GitHub</span>
+       
+
+              <span className='navbar-font'><FaGithub/>GitHub</span>
             </a>
             <a
               className='has-text-white bold nav-link-margin'
               target='_blank'
               href='https://www.linkedin.com/in/karen-astell/'
             >
-              <span className='icon'>
-                <i className='fab fa-linkedin'></i>
-              </span>
-              <span className='navbar-font'>LinkedIn</span>
+           
+              <span className='navbar-font'><FaLinkedin/>LinkedIn</span>
             </a>
             <a
-            className='has-text-white bold is-inverted'
-            href={Resume}
-            target='_blank'
-          >
-            <span className='icon'>
-              <i className='far fa-file-pdf'></i>
-            </span>
-            <span className='navbar-font'>Resume</span>
-          </a>
+              className='has-text-white bold is-inverted'
+              href={Resume}
+              target='_blank'
+            >
+            
+              <span className='navbar-font'><FaFilePdf/>Resume</span>
+            </a>
           </span>
         </div>
       </div>
