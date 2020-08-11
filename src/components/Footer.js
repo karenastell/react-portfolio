b4847d7219e23f8db98c8577ac6b9305690d5eb4
Karen Astell <KarenAstell@gmail.com>
2020-08-11 12:00:59 -0400

    icons for footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,18 @@
 import React from 'react'
+import { FaRegCircle } from "react-icons/fa";  // Font Awesome
+
 
 export default function Footer() {
     return (
         <div className="foot foot-font" role="navigation" aria-label="main navigation">
         <div className="content has-text-centered">
           <p>
-            <strong className="white-font">Karen Astell</strong>
-            <i className="far fa-circle white-font"></i>
-            <span className="white-font bold">(603) 770 7658</span>
-            <i className="far fa-circle white-font"></i>
+            <strong className="has-text-white mr-2">Karen Astell</strong><FaRegCircle style={{color: 'white'}}/>
+            <i className="far fa-circle has-text-white"></i>
+            <span className="has-text-white bold mr-2 ml-2">(603) 770 7658 </span><FaRegCircle style={{color: 'white'}}/>
+            <i className="far fa-circle has-text-white"></i>
             <a href="mailto:karenastell@gmail.com" target="_blank"
-              ><span className="white-font bold">KarenAstell@gmail.com</span>
+              ><span className="has-text-white ml-2 bold">KarenAstell@gmail.com</span>
             </a>
           </p>
         </div>
